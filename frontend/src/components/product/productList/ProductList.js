24ec178d66a86e225203e329dca4e8e157ddbbd9
Kacper <kacpeeer.mikolajczyk@gmail.com>
2024-05-23 18:34:44 +0200

    frontend - search, pagination, summary

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './productList.scss'
-import { SpinnerImg } from "../../loader/Loader";
-import { FaEdit, FaTrashAlt } from "react-icons/fa";
-import { AiOutlineEye } from "react-icons/ai";
+import { SpinnerImg } from '../../loader/Loader'
+import { FaEdit, FaTrashAlt } from 'react-icons/fa'
+import { AiOutlineEye } from 'react-icons/ai'
+import Search from '../../search/Search'
+import { useDispatch, useSelector } from 'react-redux'
+import { FILTER_PRODUCTS, selectFilteredProducts } from '../../../redux/features/product/filterSlice'
+import ReactPaginate from 'react-paginate'
 
 const ProductList = ({ products, isLoading }) => {
-    const shortenText = (text, n) => {
-        if(text.length > n) {
-            const shortenedText = text.substring(0, n).concat("...")
-            return shortenedText
-        }
-        return text
-    }
+	const [search, setSearch] = useState('')
+	const filteredProducts = useSelector(selectFilteredProducts)
+
+	const dispatch = useDispatch()
+
+	const shortenText = (text, n) => {
+		if (text.length > n) {
+			const shortenedText = text.substring(0, n).concat('...')
+			return shortenedText
+		}
+		return text
+	}
+
+	// Start pagination
+	const [currentItems, setCurrentItems] = useState([])
+	const [pageCount, setPageCount] = useState(0)
+	const [itemOffset, setItemOffset] = useState(0)
+	const itemsPerPage = 2
+
+	useEffect(() => {
+		const endOffset = itemOffset + itemsPerPage
+
+		setCurrentItems(filteredProducts.slice(itemOffset, endOffset))
+		setPageCount(Math.ceil(filteredProducts.length / itemsPerPage))
+	}, [itemOffset, itemsPerPage, filteredProducts])
+
+	const handlePageClick = event => {
+		const newOffset = (event.selected * itemsPerPage) % filteredProducts.length
+		setItemOffset(newOffset)
+	}
+	// End pagination
+
+	useEffect(() => {
+		dispatch(FILTER_PRODUCTS({ products, search }))
+	}, [products, search, dispatch])
+
 	return (
 		<div className='product-list'>
 			<hr />
@@ -20,54 +53,80 @@ const ProductList = ({ products, isLoading }) => {
 					<span>
 						<h3>Pozycje sklepu</h3>
 					</span>
-                    <span>
-                        <h3>Wyszukiwany produkt</h3>
-                    </span>
+					<span>
+						<Search
+							value={search}
+							onChange={e => setSearch(e.target.value)}
+						/>
+					</span>
 				</div>
-                {isLoading && <SpinnerImg/>}
-                <div className="table">
-                    {!isLoading && products.length === 0 ? (
-                        <p>-- Produkt nie istnieje. Dodaj produkt.</p>
-                    ): (
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Numer</th>
-                                    <th>Nazwa</th>
-                                    <th>Kategoria</th>
-                                    <th>Cena</th>
-                                    <th>Ilość</th>
-                                    <th>Wartość</th>
-                                    <th>Akcja</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    products.map((product, index) => {
-                                        const {_id, name, category, price, quantity} = product
-                                        return (
-                                            <tr key={_id}>
-                                                <td>{index + 1}</td>
-                                                <td>
-                                                    {shortenText(name, 16)}
-                                                </td>
-                                                <td>{category}</td>
-                                                <td>{price} {"PLN"}</td>
-                                                <td>{quantity}</td>
-                                                <td>{price * quantity} {"PLN"}</td>
-                                                <td className='icons'>
-                                                        <AiOutlineEye size={25} color={"purple"}/>
-                                                        <FaEdit  size={20} color={"green"}/>
-                                                        <FaTrashAlt size={20} color={"red"}/>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    )}
-                </div>
+				{isLoading && <SpinnerImg />}
+				<div className='table'>
+					{!isLoading && products.length === 0 ? (
+						<p>-- Produkt nie istnieje. Dodaj produkt.</p>
+					) : (
+						<table>
+							<thead>
+								<tr>
+									<th>Numer</th>
+									<th>Nazwa</th>
+									<th>Kategoria</th>
+									<th>Cena</th>
+									<th>Ilość</th>
+									<th>Wartość</th>
+									<th>Akcja</th>
+								</tr>
+							</thead>
+							<tbody>
+								{currentItems.map((product, index) => {
+									const { _id, name, category, price, quantity } = product
+									return (
+										<tr key={_id}>
+											<td>{index + 1}</td>
+											<td>{shortenText(name, 16)}</td>
+											<td>{category}</td>
+											<td>
+												{price} {'PLN'}
+											</td>
+											<td>{quantity}</td>
+											<td>
+												{price * quantity} {'PLN'}
+											</td>
+											<td className='icons'>
+												<AiOutlineEye
+													size={25}
+													color={'purple'}
+												/>
+												<FaEdit
+													size={20}
+													color={'green'}
+												/>
+												<FaTrashAlt
+													size={20}
+													color={'red'}
+												/>
+											</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
+					)}
+				</div>
+				<ReactPaginate
+					breakLabel='...'
+					nextLabel='Następna >'
+					onPageChange={handlePageClick}
+					pageRangeDisplayed={2}
+					pageCount={pageCount}
+					previousLabel='< Poprzednia'
+					renderOnZeroPageCount={null}
+					containerClassName='pagination'
+					pageLinkClassName='page-num'
+					previousLinkClassName='page-num'
+					nextLinkClassName='page-num'
+					activeLinkClassName='activePage'
+				/>
 			</div>
 		</div>
 	)
--- frontend/src/components/product/productSummary/ProductSummary.js
+++ frontend/src/components/product/productSummary/ProductSummary.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import './ProductSummary.scss'
+import { AiFillDollarCircle } from 'react-icons/ai'
+import { BsCart4, BsCartX } from 'react-icons/bs'
+import { BiCategory } from 'react-icons/bi'
+import InfoBox from '../../infoBox/InfoBox'
 
-const ProductSummary = () => {
-  return (
-    <div>ProductSummary</div>
-  )
+// Icons
+const earningIcon = (
+	<AiFillDollarCircle
+		size={40}
+		color='#fff'
+	/>
+)
+const productIcon = (
+	<BsCart4
+		size={40}
+		color='#fff'
+	/>
+)
+const categoryIcon = (
+	<BiCategory
+		size={40}
+		color='#fff'
+	/>
+)
+const outOfStockIcon = (
+	<BsCartX
+		size={40}
+		color='#fff'
+	/>
+)
+
+const ProductSummary = ({ products }) => {
+	return (
+    <div className="product-summary">
+    <h3 className="--mt">Statystyki</h3>
+    <div className="info-summary">
+      <InfoBox
+        icon={productIcon}
+        title={"Wszystkie produkty"}
+        count={products.length}
+        bgColor="card1"
+      />
+      <InfoBox
+        icon={earningIcon}
+        title={"Całkowita wartość"}
+        count={"0"}
+        bgColor="card2"
+      />
+      <InfoBox
+        icon={outOfStockIcon}
+        title={"Brak na stanie"}
+        count={"0"}
+        bgColor="card3"
+      />
+      <InfoBox
+        icon={categoryIcon}
+        title={"Ilość kategorii"}
+        count={"0"}
+        bgColor="card4"
+      />
+			</div>
+		</div>
+	)
 }
 
 export default ProductSummary
