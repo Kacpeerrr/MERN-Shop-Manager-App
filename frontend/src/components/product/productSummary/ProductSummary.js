8eef44888226e259a56d77eb7dbc31ee45e2c619
Kacper <kacpeeer.mikolajczyk@gmail.com>
2024-05-23 19:17:32 +0200

    frontend - calculate logic for summary

--- frontend/src/components/product/productSummary/ProductSummary.js
+++ frontend/src/components/product/productSummary/ProductSummary.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ProductSummary.scss'
 import { AiFillDollarCircle } from 'react-icons/ai'
 import { BsCart4, BsCartX } from 'react-icons/bs'
 import { BiCategory } from 'react-icons/bi'
 import InfoBox from '../../infoBox/InfoBox'
+import { useDispatch, useSelector } from 'react-redux'
+import { CALC_OUTOFSTOCK, CALC_STORE_VALUE, CALC_CATEGORY, selectTotalStoreValue, selectOutOfStock, selectCategory } from '../../../redux/features/product/productSlice'
+
+
 
 // Icons
 const earningIcon = (
@@ -31,7 +35,25 @@ const outOfStockIcon = (
 	/>
 )
 
+
+// Format Amount
+export const formatNumbers = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
+
 const ProductSummary = ({ products }) => {
+
+  const dispatch = useDispatch()
+  const totalStoreValue = useSelector(selectTotalStoreValue)
+  const outOfStock = useSelector(selectOutOfStock)
+  const category = useSelector(selectCategory)
+
+  useEffect(() => {
+    dispatch(CALC_STORE_VALUE(products))
+    dispatch(CALC_OUTOFSTOCK(products))
+    dispatch(CALC_CATEGORY(products))
+  }, [dispatch, products])
+
 	return (
     <div className="product-summary">
     <h3 className="--mt">Statystyki</h3>
@@ -45,19 +67,19 @@ const ProductSummary = ({ products }) => {
       <InfoBox
         icon={earningIcon}
         title={"Całkowita wartość"}
-        count={"0"}
+        count={`${formatNumbers(totalStoreValue.toFixed(2))} PLN`}
         bgColor="card2"
       />
       <InfoBox
         icon={outOfStockIcon}
         title={"Brak na stanie"}
-        count={"0"}
+        count={outOfStock}
         bgColor="card3"
       />
       <InfoBox
         icon={categoryIcon}
         title={"Ilość kategorii"}
-        count={"0"}
+        count={category.length}
         bgColor="card4"
       />
 			</div>
--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -9,6 +9,9 @@ const initialState = {
 	isSuccess: false,
 	isLoading: false,
 	message: '',
+	totalStoreValue: 0,
+	outOfStock: 0,
+	category: [],
 }
 
 // Create New Product
@@ -40,49 +43,87 @@ const productSlice = createSlice({
 	initialState,
 	reducers: {
 		CALC_STORE_VALUE(state, action) {
-			console.log('store value')
+			const products = action.payload
+			const array = []
+			products.map(item => {
+				const { price, quantity } = item
+				const productValue = price * quantity
+				return array.push(productValue)
+			})
+			const totalValue = array.reduce((a, b) => {
+				return a + b
+			}, 0)
+			state.totalStoreValue = totalValue
+		},
+		CALC_OUTOFSTOCK(state, action) {
+			const products = action.payload
+			const array = []
+			products.map(item => {
+				const { quantity } = item
+				return array.push(quantity)
+			})
+			let count = 0
+			array.forEach(number => {
+				if (number === 0 || number === '0') {
+					count += 1
+				}
+			})
+			state.outOfStock = count
+		},
+		CALC_CATEGORY(state, action) {
+			const products = action.payload
+			const array = []
+			products.map(item => {
+				const { category } = item
+				return array.push(category)
+			})
+            const uniqueCategory = [...new Set(array)]
+            state.category = uniqueCategory
 		},
 	},
 	extraReducers: builder => {
-        builder 
-            .addCase(createProduct.pending, (state) => {
-                state.isLoading = true
-            })
-            .addCase(createProduct.fulfilled, (state, action) => {
-                state.isLoading = false
-                state.isSuccess = true
+		builder
+			.addCase(createProduct.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(createProduct.fulfilled, (state, action) => {
+				state.isLoading = false
+				state.isSuccess = true
 				state.isError = false
-                console.log(action.payload);
-                state.products.push(action.payload)
-                toast.success("Produkt dodany pomyślnie")
-            })
-            .addCase(createProduct.rejected, (state, action) => {
-                state.isLoading = false
-                state.isError = true
-                state.message = action.payload
-                toast.error(action.payload)
-            })
-            .addCase(getProducts.pending, (state) => {
-                state.isLoading = true
-            })
-            .addCase(getProducts.fulfilled, (state, action) => {
-                state.isLoading = false
-                state.isSuccess = true
-                state.isError = false
-                console.log(action.payload);
-                state.products = action.payload
-            })
-            .addCase(getProducts.rejected, (state, action) => {
-                state.isLoading = false
-                state.isError = true
-                state.message = action.payload
-                toast.error(action.payload)
-            })
-    },
+				console.log(action.payload)
+				state.products.push(action.payload)
+				toast.success('Produkt dodany pomyślnie')
+			})
+			.addCase(createProduct.rejected, (state, action) => {
+				state.isLoading = false
+				state.isError = true
+				state.message = action.payload
+				toast.error(action.payload)
+			})
+			.addCase(getProducts.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(getProducts.fulfilled, (state, action) => {
+				state.isLoading = false
+				state.isSuccess = true
+				state.isError = false
+				console.log(action.payload)
+				state.products = action.payload
+			})
+			.addCase(getProducts.rejected, (state, action) => {
+				state.isLoading = false
+				state.isError = true
+				state.message = action.payload
+				toast.error(action.payload)
+			})
+	},
 })
 
-export const { CALC_STORE_VALUE } = productSlice.actions
+export const { CALC_STORE_VALUE, CALC_OUTOFSTOCK, CALC_CATEGORY } = productSlice.actions
 
-export const selectIsLoading = (state) => state.product.isLoading
+export const selectIsLoading = state => state.product.isLoading
+export const selectTotalStoreValue = state => state.product.totalStoreValue
+export const selectOutOfStock = state => state.product.outOfStock
+export const selectCategory = state => state.product.category
 
 export default productSlice.reducer
